623f29bb29dc73a25546135511a01f7a5f8fdc7e
Mosolov Sergey <mosolovs1987@gmail.com>
2020-08-12 22:54:43 +0400

    print call tree for amount 11 and 5 coin types

--- 1_14.scm
+++ 1_14.scm
@@ -1,7 +1,7 @@
-(define cnt 0)
+; (define cnt 0)
 
 (define (count-change amount)
-    (cc amount 5))
+    (cc amount 5 0))
 
 (define (first-denomination kinds-of-coins)
     (cond ((= kinds-of-coins 1) 1)
@@ -10,15 +10,14 @@
           ((= kinds-of-coins 4) 25)
           ((= kinds-of-coins 5) 50)))
 
-(define (cc amount kinds-of-coins)
-    (set! cnt (+ cnt 1))
+(define (cc amount kinds-of-coins level)
+    (display (string (make-string (* 4 level) #\space) "cc " (number->string amount) " : " (number->string kinds-of-coins) "\n"))
     (cond ((= amount 0) 1)
           ((or (< amount 0) (= kinds-of-coins 0)) 0)
           (else (+ (cc amount
-                       (- kinds-of-coins 1))
+                       (- kinds-of-coins 1) (+ level 1))
                    (cc (- amount
                             (first-denomination kinds-of-coins))
-                            kinds-of-coins)))))
+                            kinds-of-coins (+ level 1))))))
 
-(count-change 500)
-(display cnt)
+(count-change 11)
