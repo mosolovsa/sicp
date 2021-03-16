9047fc9a9ff339eaa2d1c3f0c8a270ae5ad009e5
Mosolov_S <mosolovs1987@gmail.com>
2021-03-16 21:58:37 +0400

    filter implementation missing, fixup that

--- chapter_2/2_20.scm
+++ chapter_2/2_20.scm
@@ -1,13 +1,12 @@
-(define (filter pred l)    
-)
+(define (my-filter pred l)
+    (cond ((null? l) '())
+          ((pred (car l)) (cons (car l) (my-filter pred (cdr l))))
+          (else (my-filter pred (cdr l)))))
 
 (define (same-parity h . t)
     (define pred (if (even? h) even? odd?))
-    (cons h (filter pred t)))
-
+    (cons h (my-filter pred t)))
 
 (same-parity 1 2 3 4 5 6 7)
-; (1 3 5 7)
 
 (same-parity 2 3 4 5 6 7)
-; (2 4 6)
